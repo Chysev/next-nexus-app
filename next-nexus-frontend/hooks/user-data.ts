2bb2a98793832af5df0e2525558aff62e8f2b9f2
Chysev <Johnlayda92@gmail.com>
2024-07-29 06:51:55 +0800

    feat: router

--- next-nexus-frontend/hooks/user-data.ts
+++ next-nexus-frontend/hooks/user-data.ts
@@ -1,10 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
 import { GetUserData } from "@/app/api/users";
+import { useQuery } from "@tanstack/react-query";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-const UserData = (sessionToken: string) => {
+const UserData = (sessionToken: string, router?: AppRouterInstance) => {
   return useQuery({
     queryKey: ["User", "Profile_Data"],
-    queryFn: async () => await GetUserData(sessionToken),
+    queryFn: async () => await GetUserData(sessionToken, router),
+    refetchInterval: 3000,
   });
 };
 
