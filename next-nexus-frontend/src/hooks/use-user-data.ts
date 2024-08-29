73d435676d6b79154aa3b39bfd085244c0879bd8
Chysev <Johnlayda92@gmail.com>
2024-08-29 22:03:31 +0800

    fix: naming convention

--- next-nexus-frontend/src/hooks/use-user-data.ts
+++ next-nexus-frontend/src/hooks/use-user-data.ts
@@ -1,13 +1,13 @@
-import { GetUserData } from "@/app/api/users";
 import { useQuery } from "@tanstack/react-query";
+import { getUserSessionData } from "@/app/api/users/index.c";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-const UserData = (sessionToken: string, router?: AppRouterInstance) => {
+const useUserData = (sessionToken: string, router?: AppRouterInstance) => {
   return useQuery({
     queryKey: ["User", "Profile_Data"],
-    queryFn: async () => await GetUserData(sessionToken, router),
+    queryFn: async () => await getUserSessionData(sessionToken, router),
     refetchInterval: 3000,
   });
 };
 
-export default UserData;
+export default useUserData;
