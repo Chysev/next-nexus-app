d6d5c50c684c593aaa39825d75846748894e4bc1
Chysev <Johnlayda92@gmail.com>
2024-08-29 22:05:48 +0800

    fix & feat: env and nexus key

--- next-nexus-frontend/src/lib/Axios.ts
+++ next-nexus-frontend/src/lib/Axios.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "nexus-key": process.env.NEXT_PUBLIC_NEXUS_KEY,
   },
 });
 
