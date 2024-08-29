de67ca76a9b6f493553569d0433cc82ef94a77ca
Chysev <Johnlayda92@gmail.com>
2024-08-29 22:07:21 +0800

    feat: validators for new forms

--- next-nexus-frontend/src/validators/index.ts
+++ next-nexus-frontend/src/validators/index.ts
@@ -18,14 +18,65 @@ const LoginSchema = z.object({
 });
 
 const UpdateUserDataSchema = z.object({
-  name: z.string().min(2, {
-    message: "Name must be at least 2 characters.",
-  }),
-  email: z.string().email({ message: "Please enter a valid email address" }),
-  password: z.string().min(8, {
-    message: "Password must be at least 8 characters",
-  }),
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .optional(),
+  email: z
+    .string()
+    .email({ message: "Please enter a valid email address" })
+    .optional(),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters",
+    })
+    .optional(),
+  role: z.string().optional(),
+  avatarUrl: z.string().optional(),
   description: z.string().optional(),
 });
 
-export { RegisterSchema, LoginSchema, UpdateUserDataSchema };
+const UpdateViewUserDataSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .optional(),
+  email: z
+    .string()
+    .email({ message: "Please enter a valid email address" })
+    .optional(),
+  role: z.string().optional(),
+});
+
+const ForgotResetPasswordSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address" }),
+});
+
+const ResetPasswordSchema = z.object({
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
+const MailConfigSchema = z.object({
+  smtpHost: z.string().min(1, "SMTP Host is required"),
+  smtpPort: z.string().min(1, "SMTP Host is required"),
+  smtpService: z.string().min(1, "SMTP Service is required"),
+  smtpEmail: z.string().email("Invalid email address"),
+  smtpPassword: z.string().min(1, "App Password is required"),
+});
+
+export {
+  LoginSchema,
+  RegisterSchema,
+  MailConfigSchema,
+  ResetPasswordSchema,
+  UpdateUserDataSchema,
+  UpdateViewUserDataSchema,
+  ForgotResetPasswordSchema,
+};
