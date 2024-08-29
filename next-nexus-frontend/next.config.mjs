d0160ce478db99ca68cfa7f52f9dd1d0b57757f8
Chysev <Johnlayda92@gmail.com>
2024-08-29 22:09:20 +0800

    update: standalone optional

--- next-nexus-frontend/next.config.mjs
+++ next-nexus-frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  // Enable this if you are going to use Docker
+  // output: "standalone",
 };
 
 export default nextConfig;
