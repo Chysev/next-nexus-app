9292cbb04c9562acb8cac61639f034f8ffb6b17f
Chysev <Johnlayda92@gmail.com>
2024-09-06 06:03:18 +0800

    fix: moved to client api

--- next-nexus-frontend/src/app/api/auth/index.c.ts
+++ next-nexus-frontend/src/app/api/auth/index.c.ts
@@ -1,18 +1,13 @@
 "use client";
+import {
+  LoginSchema,
+  RegisterSchema,
+  ResetPasswordSchema,
+  ForgotResetPasswordSchema,
+} from "@/validators";
 import { z } from "zod";
 import Axios from "@/lib/Axios";
 import { NextResponse } from "next/server";
-import { ResetPasswordSchema } from "@/validators";
-
-const Logout = async () => {
-  try {
-    const response = await Axios.get("/api/v1/auth/logout");
-
-    return response;
-  } catch (error) {
-    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
-  }
-};
 
 const ResetPassword = async (
   datas: z.infer<typeof ResetPasswordSchema>,
@@ -30,4 +25,41 @@ const ResetPassword = async (
   }
 };
 
-export { Logout, ResetPassword };
+const Login = async (datas: z.infer<typeof LoginSchema>) => {
+  try {
+    const response = await Axios.post("/api/v1/auth/login", datas);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      return {
+        status: error.response.status,
+        message: error.response.data.message,
+      };
+    }
+    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
+  }
+};
+
+const Register = async (datas: z.infer<typeof RegisterSchema>) => {
+  try {
+    const response = await Axios.post("/api/v1/auth/register", datas);
+
+    return response.data;
+  } catch (error) {
+    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
+  }
+};
+
+const ForgotResetPassword = async (
+  datas: z.infer<typeof ForgotResetPasswordSchema>
+) => {
+  try {
+    const response = await Axios.post("/api/v1/auth/forgot-password", datas);
+
+    return response.data;
+  } catch (error) {
+    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
+  }
+};
+
+export { Login, Register, ForgotResetPassword, ResetPassword };
--- next-nexus-frontend/src/app/api/auth/index.s.ts
+++ next-nexus-frontend/src/app/api/auth/index.s.ts
@@ -1,11 +1,7 @@
 "use server";
-import {
-  LoginSchema,
-  RegisterSchema,
-  ForgotResetPasswordSchema,
-} from "@/validators";
 import { z } from "zod";
 import Axios from "@/lib/Axios";
+import { LoginSchema } from "@/validators";
 import { NextResponse } from "next/server";
 
 const Login = async (datas: z.infer<typeof LoginSchema>) => {
@@ -23,26 +19,4 @@ const Login = async (datas: z.infer<typeof LoginSchema>) => {
   }
 };
 
-const Register = async (datas: z.infer<typeof RegisterSchema>) => {
-  try {
-    const response = await Axios.post("/api/v1/auth/register", datas);
-
-    return response.data;
-  } catch (error) {
-    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
-  }
-};
-
-const ForgotResetPassword = async (
-  datas: z.infer<typeof ForgotResetPasswordSchema>
-) => {
-  try {
-    const response = await Axios.post("/api/v1/auth/forgot-password", datas);
-
-    return response.data;
-  } catch (error) {
-    return new NextResponse("INTERNAL_SERVER_ERROR", { status: 500 });
-  }
-};
-
-export { Login, Register, ForgotResetPassword };
+export { Login };
