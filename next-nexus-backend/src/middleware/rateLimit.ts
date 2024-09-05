b93c9875b7c85ee8ea1624d4d87e5fa5874ee380
Chysev <Johnlayda92@gmail.com>
2024-09-06 05:57:56 +0800

    fix: limiter by ip

--- next-nexus-backend/src/middleware/rateLimit.ts
+++ next-nexus-backend/src/middleware/rateLimit.ts
@@ -1,19 +1,40 @@
-import rateLimit from "express-rate-limit";
-import RedisStore from "rate-limit-redis";
 import redisClient from "@/lib/redisClient";
+import { Request, Response, NextFunction } from "@/types/express-types";
 
-const createRateLimiter = (maxRequests: number) =>
-  rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: maxRequests,
-    store: new RedisStore({
-      sendCommand: (...args) => redisClient.sendCommand(args),
-    }),
-    standardHeaders: true,
-    legacyHeaders: false,
-  });
+const rateLimiter = (rule: RateLimiterRule) => {
+  const { rate_limit } = rule;
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const ip = req.ip;
+    const redisId = `${req.path}/${ip}`;
 
-const authRateLimiter = createRateLimiter(10);
-const generalRateLimiter = createRateLimiter(10000);
+    const requestCount = await redisClient.incr(redisId);
+    if (requestCount === 1) {
+      await redisClient.expire(redisId, rate_limit.time);
+    }
 
-export { generalRateLimiter, authRateLimiter };
+    if (requestCount > rate_limit.limit) {
+      return res.status(429).json({
+        data: { message: "Too many requests, try again later." },
+      });
+    }
+
+    next();
+  };
+};
+
+const dataRateLimiter: RateLimiterRule = {
+  rate_limit: {
+    time: 60,
+    limit: 10,
+  },
+};
+
+const generalRateLimiter: RateLimiterRule = {
+  rate_limit: {
+    time: 60,
+    limit: 1000,
+  },
+};
+
+export { dataRateLimiter, generalRateLimiter };
+export default rateLimiter;
